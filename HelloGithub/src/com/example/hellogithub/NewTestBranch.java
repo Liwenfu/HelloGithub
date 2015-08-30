05671466a37b0728f6ac95eef81cd78f8c4de7e5
Liwenfu <905865563@qq.com>
2015-08-30 14:58:01 +0800

    new branch
    输出new test of github branch

---
+++ HelloGithub/src/com/example/hellogithub/NewTestBranch.java
@@ -0,0 +1,9 @@
+package com.example.hellogithub;
+
+public class NewTestBranch {
+	public NewTestBranch() {
+		// TODO Auto-generated constructor stub
+		System.out.println("new test of github branch");
+	}
+
+}
