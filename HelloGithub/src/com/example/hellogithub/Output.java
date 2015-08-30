a9a72aa17cb054d37c44baeb7d2b435ea7491d7f
Liwenfu <905865563@qq.com>
2015-08-30 14:11:15 +0800

    输出
    输出偶数

---
+++ HelloGithub/src/com/example/hellogithub/Output.java
@@ -0,0 +1,11 @@
+package com.example.hellogithub;
+
+public class Output {
+
+	public static void outputOushu(int n){
+		for(int i=0;i<=n;i++){
+			if(i%2==0)
+				System.out.println(n);
+		}
+	}
+}
